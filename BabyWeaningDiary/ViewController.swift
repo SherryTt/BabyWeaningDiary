c963feaab7bbcdd847582a5b4196437ab3a90f0e
Shiori <shiori0921syd@gmail.com>
2022-11-13 23:21:53 +1100

    Initial Commit

---
+++ BabyWeaningDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BabyWeaningDiary
+//
+//  Created by Shiori on 13/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
